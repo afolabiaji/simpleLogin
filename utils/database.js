c2e36e99dffb977fae31e93f1de5b81eb8217ddc
afolabiaji <af.ajibade@gmail.com>
2020-10-25 15:37:17 +0000

    hidden credentials

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('simpleLogin', 'postgres', '+vw(Es!v>DJG2ZH!', {
-    host: 'database-1.c4c2lkelghen.eu-west-2.rds.amazonaws.com',
+const sequelize = new Sequelize('DB_NAME', 'DB_USER', 'DB_PASSWORD', {
+    host: 'DB_ENDPOINT',
     dialect: 'postgres'
 })
 
